44c356c2175d1f62764cc9bf56dca38f8aa8527b
AnimewkaSes <brol0526@gmail.com>
2022-12-13 00:56:48 +0600

    Задача 41

---
+++ Ex041/Program.cs
@@ -0,0 +1,29 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.Write("Введите размер массива: ");
+        int n = Convert.ToInt32(Console.ReadLine());
+        int [] arr = new int[n];
+        int result = 0;
+        Console.WriteLine("Заполните массив: ");
+        for (int i = 0; i < arr.Length; i++)
+        {
+            arr[i] = Convert.ToInt32(Console.ReadLine());
+        }
+        Console.Write("Введённый массив: ");
+        for (int i = 0; i < arr.Length; i++)
+        {
+            Console.Write("{0} ",arr[i]);
+        }
+
+        for (int i = 0; i < arr.Length; i++)
+        {
+            if (arr[i]>0)
+            {
+                result +=1;
+            }
+        }
+        Console.WriteLine("\nКоличество чисел > 0 : {0}", result);
+    }
+}
