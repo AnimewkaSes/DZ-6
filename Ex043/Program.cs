732a68c770c39b6da113273f160c06e5fdbde68d
AnimewkaSes <brol0526@gmail.com>
2022-12-13 22:58:01 +0600

    Задача 43

---
+++ Ex043/Program.cs
@@ -0,0 +1,25 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.WriteLine("Программа для поиска перечесения двух прямых");
+        double x,y;
+        Console.WriteLine("Координаты первой прямой линии");
+        Console.Write("Введите b1: ");
+        double b1 = Convert.ToDouble(Console.ReadLine());
+        Console.Write("Введите k1: ");
+        double k1 = Convert.ToDouble(Console.ReadLine());
+
+        Console.WriteLine("Координаты второй прямой линии");
+        Console.Write("Введите b2: ");
+        double b2 = Convert.ToDouble(Console.ReadLine());
+        Console.Write("Введите k2: ");
+        double k2 = Convert.ToDouble(Console.ReadLine());
+
+       x = Math.Round((-b2+b1)/(-k1+k2),2);
+       
+       y = Math.Round(k2*x+b2,2);
+       
+       Console.Write("Точка пересечения двух линий: ({0};{1})",x,y);
+    }
+}
